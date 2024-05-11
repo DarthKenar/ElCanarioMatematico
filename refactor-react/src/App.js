ca24c055af9f4225ca11e249e44936b83711d0cb
Federico Vega PC <federico.vega2222@gmail.com>
2024-05-10 21:31:53 -0300

    🚧 Work in Progress:  Delimitar donde ira el contenido de la extension

--- refactor-react/src/App.js
+++ refactor-react/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import * as React from 'react'
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Container } from '@chakra-ui/react'
 
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
@@ -12,6 +12,9 @@ function App() {
   return (
     <ChakraProvider>
       <Header/>
+      <Container maxW='2xl'>
+        Contenido de la extension
+      </Container>
       <Footer/>
     </ChakraProvider>
   )
